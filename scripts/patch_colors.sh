e608dfdd571f65db0ded4138025a9e86db13add5
Adil Hanney <adilhanney@disroot.org>
2025-08-16 23:54:56 +0100

    Less dark colors

--- scripts/patch_colors.sh
+++ scripts/patch_colors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Make backgrounds darker in dark mode
-sed -i 's/#36363a/#131313/g' build/patched/gnome-shell-sass/_colors.scss
-sed -i 's/#222226/#101010/g' build/patched/gnome-shell-sass/_default-colors.scss
+sed -i 's/#36363a/#181818/g' build/patched/gnome-shell-sass/_colors.scss
+sed -i 's/#222226/#111111/g' build/patched/gnome-shell-sass/_default-colors.scss
