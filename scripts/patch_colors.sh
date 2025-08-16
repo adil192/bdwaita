6b401dde1da1fb95cfde290e5f9db14016adc530
Adil Hanney <adilhanney@disroot.org>
2025-08-16 23:47:07 +0100

    Darken mixed bg colors

--- scripts/patch_colors.sh
+++ scripts/patch_colors.sh
@@ -2,3 +2,4 @@
 
 # Make backgrounds darker in dark mode
 sed -i 's/#36363a/#131313/g' build/patched/gnome-shell-sass/_colors.scss
+sed -i 's/#222226/#101010/g' build/patched/gnome-shell-sass/_default-colors.scss
