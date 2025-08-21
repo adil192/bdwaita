5d8730cb5b29fdf1155505bf4aa5a3d964bb2a50
Adil Hanney <adilhanney@disroot.org>
2025-08-21 17:04:02 +0100

    Purple colors

--- src/patch_colors.sh
+++ src/patch_colors.sh
@@ -3,8 +3,8 @@
 # Replace some greys with darker greys
 for file in $(find build/patched/ -type f -name "*.scss"); do
   sed -i \
-    -e 's/#3d3846/#1f1f1f/g' \
-    -e 's/#36363a/#181818/g' \
-    -e 's/#222226/#111111/g' \
+    -e 's/#3d3846/#201f24/g' \
+    -e 's/#36363a/#161322/g' \
+    -e 's/#222226/#000920/g' \
     "$file"
 done
