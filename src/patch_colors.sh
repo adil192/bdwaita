6fe2d1f370c5b96aea5c8e33836f1236d41f9ff8
Adil Hanney <adilhanney@disroot.org>
2026-05-30 12:36:37 +0100

    ref: use while read instead of for in find
    https://www.shellcheck.net/wiki/SC2044

--- src/patch_colors.sh
+++ src/patch_colors.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Replace some greys with darker greys
-for file in $(find build/patched/ -type f -name "*.scss"); do
+while IFS= read -r -d '' file; do
   sed -i \
     -e 's/#3d3846/#1f1f1f/g' \
     -e 's/#36363a/#181818/g' \
     -e 's/#222226/#111111/g' \
     "$file"
-done
+done < <(find build/patched/ -type f -name "*.scss" -print0)
